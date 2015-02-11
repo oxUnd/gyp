2a12138dd87eebe63915c9f35f0c9dadb488d51e
xiangshouding <fansekey@gmail.com>
2015-02-11 20:56:41 +0800

    fix

--- third-party.sh
+++ third-party.sh
@@ -4,6 +4,10 @@
 wget "http://jaist.dl.sourceforge.net/project/libpng/libpng16/1.6.16/libpng-1.6.16.tar.gz" -O "./libpng.tar.gz"
 tar xvf libpng.tar.gz
 mv -f libpng-1.6.16 ./third-party/libpng
+#fix Linux compile error
+cd ./third-party/libpng/
+    cp ./scripts/pnglibconf.h.prebuilt ./pnglibconf.h
+cd -
 rm libpng.tar.gz
 
 ## libjpeg-turbo ##
@@ -11,7 +15,7 @@ wget "http://jaist.dl.sourceforge.net/project/libjpeg-turbo/1.4.0/libjpeg-turbo-
 tar xvf libjpeg-turbo.tar.gz
 mv -f libjpeg-turbo-1.4.0 ./third-party/libjpeg-turbo
 cd ./third-party/libjpeg-turbo
-cmake .
+    cmake .
 cd -
 rm libjpeg-turbo.tar.gz
 
