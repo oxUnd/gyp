2546541ba1552a19039890ff0807ce7582847403
OstlerDev <ostlerdev@me.com>
2015-08-21 12:58:14 -0600

    Update third-party.sh

--- third-party.sh
+++ third-party.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 ## libpng ##
-wget "http://jaist.dl.sourceforge.net/project/libpng/libpng16/1.6.16/libpng-1.6.16.tar.gz" -O "./libpng.tar.gz"
+wget "http://downloads.sourceforge.net/project/libpng/libpng16/1.6.18/libpng-1.6.18.tar.xz" -O "./libpng.tar.gz"
 tar xvf libpng.tar.gz
-mv -f libpng-1.6.16 ./third-party/libpng
+mv -f libpng-1.6.18 ./third-party/libpng
 #fix Linux compile error
 cd ./third-party/libpng/
-    cp ./scripts/pnglibconf.h.prebuilt ./pnglibconf.h
-    # close warning
-    if [ "$(uname)" = "Linux" ]; then
-        sed -i  "s/#define PNG_WARNINGS_SUPPORTED/\/\/#define PNG_WARNINGS_SUPPORTED/" ./pnglibconf.h
-    else
-        sed -i '' "s/#define PNG_WARNINGS_SUPPORTED/\/\/#define PNG_WARNINGS_SUPPORTED/" ./pnglibconf.h
-    fi
+cp ./scripts/pnglibconf.h.prebuilt ./pnglibconf.h
+# close warning
+if [ "$(uname)" = "Linux" ]; then
+sed -i  "s/#define PNG_WARNINGS_SUPPORTED/\/\/#define PNG_WARNINGS_SUPPORTED/" ./pnglibconf.h
+else
+sed -i '' "s/#define PNG_WARNINGS_SUPPORTED/\/\/#define PNG_WARNINGS_SUPPORTED/" ./pnglibconf.h
+fi
 cd -
 rm libpng.tar.gz
 
@@ -21,12 +21,12 @@ wget "http://jaist.dl.sourceforge.net/project/libjpeg-turbo/1.4.0/libjpeg-turbo-
 tar xvf libjpeg-turbo.tar.gz
 mv -f libjpeg-turbo-1.4.0 ./third-party/libjpeg-turbo
 cd ./third-party/libjpeg-turbo
-    cmake . || (echo ':MUST: need cmake' && exit 1)
+cmake . || (echo ':MUST: need cmake' && exit 1)
 cd -
 rm libjpeg-turbo.tar.gz
 
 ## giflib ##
-wget "http://cznic.dl.sourceforge.net/project/giflib/giflib-4.x/giflib-4.2.3.tar.gz" -O "./giflib.tar.gz"
+wget "http://downloads.sourceforge.net/project/giflib/giflib-4.x/giflib-4.2.3.tar.gz" -O "./giflib.tar.gz"
 tar xvf giflib.tar.gz
 mv -f giflib-4.2.3 ./third-party/giflib
 rm giflib.tar.gz
